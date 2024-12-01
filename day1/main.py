41d2bc590a956cbb94230c0b1685037619c80955
Nicholas Karr <nickkarr2005@gmail.com>
2024-11-30 23:14:13 -0600

    Day 1 initial solve

--- day1/main.py
+++ day1/main.py
@@ -1,10 +1,32 @@
 import re, os, functools, sys
 
 sys.path.insert(0, '..')
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
 from util import *
 
 file = open(os.path.dirname(os.path.realpath(__file__)) + '/input')
 lines = file.readlines()
+
+lefts = []
+rights = []
+
+for line in lines:
+    nums = re.search(r'(\d*) *(\d*)', line).groups()
+    lefts.append(int(nums[0]))
+    rights.append(int(nums[1]))
+
+lefts = sorted(lefts)
+rights = sorted(rights)
+
+sum = 0
+for i in range(len(lefts)):
+    sum += abs(lefts[i] - rights[i])
+
+print(sum)
+
+score = 0
+for i in lefts:
+    cnt = rights.count(i)
+    score += i * cnt
+
+print(score)
